318d10b4da7e6adc252d9b4608930976e91c786b
Kevo <me@kevo.link>
2021-08-03 08:33:36 +0200

    a

--- scrappy_antivirus/database/convert_to_database.py
+++ scrappy_antivirus/database/convert_to_database.py
@@ -3,11 +3,11 @@ from scrappy_antivirus.database import Hash, Threat
 
 
 def add_all_checksums_to_database():
-    for checksum in get_all_checksums():
+    for i, checksum in enumerate(get_all_checksums()):
         db_threat = Threat.create(type=None, name=checksum.threat_name)
         db_checksum = Hash.create(threat=db_threat, hash_type=checksum.checksum_type, checksum=checksum.checksum)
 
-        print("Added", db_threat.name, db_checksum.checksum)
+        print(f"{i}. added:", db_threat.name, db_checksum.checksum)
 
 
 if __name__ == "__main__":
